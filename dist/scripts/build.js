e953d701125e96a45e57adc307e8f479244433f1
sPefie <sweetpirozhok@gmail.com>
2016-10-17 01:56:45 +0300

    +Popup

--- dist/scripts/build.js
+++ dist/scripts/build.js
@@ -213,13 +213,35 @@
 
 $(document).ready(function(){
 
-  $(".disabled").click(function(event) {
-  	event.preventDefault();
-  });
-  $("a[href='']").click(function(event) {
-  	event.preventDefault();
-  });
-  
+	$(".disabled").click(function(event) {
+		event.preventDefault();
+	});
+	$("a[href='']").click(function(event) {
+		event.preventDefault();
+	});
+
+
+// Magic popup
+
+
+$('.popup_img').magnificPopup({
+	type: 'image',
+	closeOnContentClick: true,
+	fixedContentPos: false,
+
+	zoom: {
+    enabled: true,
+    duration: 300, 
+    easing: 'ease-in-out',
+   opener: function(openerElement) {
+      return openerElement.is('.popup_img') ? openerElement : openerElement.find('.popup_img');
+    }
+  }
+
+});  
+
+
+
 
 });
 //
