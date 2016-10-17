46f929d43c011d11658dd2744f49a1e19efc17dd
sPefie <sweetpirozhok@gmail.com>
2016-10-17 22:58:21 +0300

    Added Isotop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,30 +1,60 @@
-$(document).ready(function(){
+/*
+*
+*  Main
+*
+*
+*/
+"use strict";
+
+
+jQuery(document).ready(function ($) {
 
 	$('body').on('click', '.disabled, a[href="#"], a[href=""]', function (event) {
 		event.preventDefault();
 	});
 
 
-// Magic popup
+	// Magic popup
+	$('.popup_img').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		fixedContentPos: true,
 
-
-$('.popup_img').magnificPopup({
-	type: 'image',
-	closeOnContentClick: true,
-	fixedContentPos: true,
-
-	zoom: {
-		enabled: true,
-		duration: 300, 
-		easing: 'ease-in-out',
-		opener: function(openerElement) {
-			return openerElement.is('.popup_img') ? openerElement : openerElement.find('.popup_img');
+		zoom: {
+			enabled: true,
+			duration: 300, 
+			easing: 'ease-in-out',
+			opener: function(openerElement) {
+				return openerElement.is('.popup_img') ? openerElement : openerElement.find('.popup_img');
+			}
 		}
-	}
 
-});  
+	});  
+
+	//Isotop
+	$(function () {
+	  // init Isotope
+	  var $grid = $('.portfolio-child').isotope({
+	  	itemSelector: '.portfolio-item',
+	  	layoutMode: 'fitRows'
+	  });
+	  // filter functions
+	  var filterFns = {};
+	  
+	  // bind filter button click
+	  $('.portfolio-menu').on('click', 'button', function () {
+	  	var filterValue = $(this).attr('data-filter');
+      filterValue = filterFns[ filterValue ] || filterValue;
+      $grid.isotope({filter: filterValue});
+
+      //add class .is-checked to current button
+      $('.portfolio-menu-item').removeClass('is-checked');
+      $(this).addClass('is-checked');
+    });
+
+	});
 
 
 
 
-});
+}); //End
